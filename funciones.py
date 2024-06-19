af34671540bb3b768eb2e614ba2b70e177c61dd4
tobal <cr.huinca@duocuc.cl>
2024-06-19 09:06:33 -0400

    funcion 1

--- funciones.py
+++ funciones.py
@@ -1,5 +1,15 @@
+contactos = []
+
 def opcion_1():
-    pass
+    print("AGREGAR CONTACTO")
+    nombre = input("Ingrese nombre :")
+    telefono = int(input("Ingrese teléfono :"))
+    correo = input("Ingrese correo: ")
+    contacto = {"nombre": nombre, 
+                "telefono": telefono,
+                "correo": correo}
+    contactos.append(contacto)
+    print("CONTACTO AÑADIO!")
 def opcion_2():
     pass
 def opcion_3():
