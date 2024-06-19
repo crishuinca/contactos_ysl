3a34911dc95c48e2920294f4ebeb2f87f406372a
tobal <cr.huinca@duocuc.cl>
2024-06-19 08:55:59 -0400

    la ná misma

---
+++ princ.py
@@ -0,0 +1,20 @@
+from funciones import *
+
+while True:
+    print(""" MENÚ CONTACTOS
+          
+          1. Agregar contacto
+          2. Mostrar contactos
+          3. Guardar archivo CSV
+          4. Salir""")
+
+    opc = int(input("Ingrese opción"))
+
+    if opc == 1:
+        pass
+    elif opc == 2:
+        pass
+    elif opc == 3:
+        pass
+    else:
+        pass
